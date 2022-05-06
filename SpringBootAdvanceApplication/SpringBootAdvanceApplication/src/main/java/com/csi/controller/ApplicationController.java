f4fc220ca3d2046b2b7076f8ae42e50b2cbfd653
KarishmaGajbhiye <1997lakhandur@gmail.com>
2022-05-06 11:51:33 +0530

    added show data service

--- SpringBootAdvanceApplication/SpringBootAdvanceApplication/src/main/java/com/csi/controller/ApplicationController.java
+++ SpringBootAdvanceApplication/SpringBootAdvanceApplication/src/main/java/com/csi/controller/ApplicationController.java
@@ -19,4 +19,12 @@ public class ApplicationController {
     {
         return "Software application developement";
     }
+
+    @GetMapping("/showdata")
+    public String sayShowData()
+    {
+        return "Product";
+    }
+
+
 }
